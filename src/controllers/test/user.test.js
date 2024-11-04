0d60e7cae4168212d33acdf8c51d57b7d888ef66
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-11-04 21:29:52 +0700

    feat:try-ci-cd-v2

--- src/controllers/test/user.test.js
+++ src/controllers/test/user.test.js
@@ -118,7 +118,7 @@ describe("Test_userController", () => {
 
       expect(res.status).toHaveBeenCalledWith(404);
       expect(res.json).toHaveBeenCalledWith({
-        message: "User not foundd",
+        message: "User not found",
       });
       expect(mockGetUserById).toHaveBeenCalledWith(req.params.id);
     });
@@ -126,7 +126,7 @@ describe("Test_userController", () => {
     test("should return 500 if service throws an error", async () => {
       const mockGetUserById = jest
         .spyOn(User.prototype, "getUserById")
-        .mockRejectedValue(new Error("Internal server errorr"));
+        .mockRejectedValue(new Error("Internal server error"));
 
       await userController.getUserById(req, res);
 
